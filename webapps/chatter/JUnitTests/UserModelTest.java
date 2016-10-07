e45433875dc1e10dd7f9ce76f5fc91b5adf39443
pranavt97 <p_thirunavukkarasu@u.pacific.edu>
2016-10-07 16:28:02 -0700

    Suite test

--- webapps/chatter/JUnitTests/UserModelTest.java
+++ webapps/chatter/JUnitTests/UserModelTest.java
@@ -2,8 +2,66 @@ import org.junit.*;
 
 public class UserModelTest
 {
+
+	//Static Tests 
+	int numUsers = 10;
+
 	@Test
-	public void testTest()
+	public void populateUserListTest()
+	{
+		String[] names = new String[numUsers];
+		String[] pass = new String[numUsers];
+
+		for(int i = 0; i < numUsers; i++)
+		{
+			names[i] = "User " + i;
+			pass[i] = "Pass " + i;
+			UserModel.addUser(new UserModel(names[i], pass[i]));
+		}
+
+		Assert.assertTrue(UserModel.lastID == numUsers);
+	}
+
+	@Test
+	public void getUserFromIDTest()
+	{
+		boolean validTest = true;
+
+		for(int i = 0; i < numUsers; i++)
+			if(!UserModel.getUser(i).getName().equals("User " + i))
+				validTest = false;
+
+		Assert.assertTrue(validTest);
+	}
+
+	@Test
+	public void getUserFromNameTest()
+	{
+		boolean validTest = true;
+
+		for(int i = 0; i < numUsers; i++)
+			if(UserModel.getUser("User " + i).getID() != i)
+				validTest = false;
+
+		Assert.assertTrue(validTest);
+	}
+
+	@Test
+	public void authUserTest()
+	{
+		boolean validTest = true;
+
+		for(int i = 0; i < numUsers; i++)
+			if(UserModel.authuser("User " + i, "Pass " + i) == null)
+				validTest = false;
+
+		Assert.assertTrue(validTest);
+
+	}
+
+	//Instance Tests
+	@Test
+	public void getNameTest()
 	{
 		String name = "name";
 		String pass = "pass";
@@ -12,4 +70,17 @@ public class UserModelTest
 
 		Assert.assertTrue(user.getName().equals(name));
 	}
+
+	@Test
+	public void getIDTest()
+	{
+		String name = "name";
+		String pass = "pass";
+		int lastID = UserModel.lastID;
+
+		UserModel user = new UserModel(name, pass); 
+
+		Assert.assertTrue(user.getID() == (lastID - 1));
+	}
+
 }

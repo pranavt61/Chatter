ecf16d419fdc26fdac488701a7ef3702e7c21318
pranavt97 <p_thirunavukkarasu@u.pacific.edu>
2016-10-07 13:11:32 -0700

    junit init

--- compileJava.sh
+++ compileJava.sh
@@ -1,3 +1,3 @@
 echo "Parameters:"
 echo "	1)web app name(folder name in /webapps)"
-javac -classpath ../lib/servlet-api.jar:webapps/$1/classes -d webapps/$1/WEB-INF/classes webapps/$1/src/*.java
+javac -classpath ../lib/*.jar:webapps/$1/classes -d webapps/$1/WEB-INF/classes webapps/$1/src/*.java
--- runContainer.sh
+++ runContainer.sh
@@ -1,6 +1,7 @@
 echo "parameters:"
 echo "	1)docker image:tag(req)"
 git pull origin master
+rm webapps/$1/WEB-INF/classes/*.class
 sh compileJava.sh $1
-sh buildContainer.sh $1
-docker run -it --rm -p 8888:8080 $1
+#if new class files are found, build and deploy container
+[ -f webapps/$1/WEB-INF/classes/*.class ] && sh buildContainer.sh $1 && docker run -it --rm -p 8888:8080 $1 || echo "Compiltion error"

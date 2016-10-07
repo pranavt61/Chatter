15457f4c52a0c91ea88e087ae3dbf9ac57acab71
pranavt97 <p_thirunavukkarasu@u.pacific.edu>
2016-10-07 13:50:38 -0700

    runContainer update

--- runContainer.sh
+++ runContainer.sh
@@ -3,5 +3,13 @@ echo "	1)docker image:tag(req)"
 git pull origin master
 rm webapps/$1/WEB-INF/classes/*.class
 sh compileJava.sh $1
+shopt -s nullglob
+classArr=(webapps/$1/WEB-INF/classes/*)
+
 #if new class files are found, build and deploy container
-[ -f webapps/$1/WEB-INF/classes/*.class ] && sh buildContainer.sh $1 && docker run -it --rm -p 8888:8080 $1 || echo "Compiltion error"
+if [ ${#classArr[@]} -eq 0]; then 
+	echo "Compiltion error"
+else
+	sh buildContainer.sh $1
+	docker run -it --rm -p 8888:8080 $1
+fi

19be520c75aec42c584ce0c6f1a3bebedd918578
pranavt97 <p_thirunavukkarasu@u.pacific.edu>
2016-10-07 13:15:57 -0700

    junit fix

--- compileJava.sh
+++ compileJava.sh
@@ -1,3 +1,3 @@
 echo "Parameters:"
 echo "	1)web app name(folder name in /webapps)"
-javac -classpath ../lib/*.jar:webapps/$1/classes -d webapps/$1/WEB-INF/classes webapps/$1/src/*.java
+javac -classpath ../lib/servlet-api.jar:../lib/junit-4.12.jar:webapps/$1/classes -d webapps/$1/WEB-INF/classes webapps/$1/src/*.java
